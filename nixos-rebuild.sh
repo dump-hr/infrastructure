dd1b176776a1f12dad307a2875d794bc6d5767ca
Bartol Deak <b@bdeak.net>
2022-10-08 22:36:12 +0200

    fix nixos rebuild switch
    - use sudo for copying `configuration.nix`
    - don't use tmux for running `nixos-rebuild switch`

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -5,5 +5,7 @@ set -ex
 ssh_connection="$1"
 configuration_file="$2"
 
-rsync "$configuration_file" "$ssh_connection:/etc/nixos/configuration.nix"
-ssh -t "$ssh_connection" "tmux new -s nixos-rebuild -d 'sudo nixos-rebuild switch'"
+rsync "$configuration_file" "$ssh_connection:/etc/nixos/configuration.nix" --rsync-path="sudo rsync"
+ssh -t "$ssh_connection" "sudo nixos-rebuild switch"
+
+# ssh -t "$ssh_connection" "tmux new -s rebuild 'sudo nixos-rebuild switch || sleep 600'"
